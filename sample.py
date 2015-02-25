a4b74f9e22dc505e7e8a73d1156067e27e7ab795
Marc Abramowitz <marc@marc-abramowitz.com>
2015-02-24 16:33:00 -0800

    Make API saner & easier to use
    Methods return data dicts instead of mutating the client and then
    returning it and expecting the user to call `get_from_server`.

--- sample.py
+++ sample.py
@@ -9,5 +9,75 @@ host = os.getenv('TEAMCITY_HOST')
 port = int(os.getenv('TEAMCITY_PORT'))
 
 tc = TeamCityRESTApiClient(user, password, host, port)
-tc.get_server_info()
-print(json.dumps(tc.get_from_server(), indent=4))
+
+# data = tc.get_all_users()
+# print(json.dumps(data, indent=4))
+
+data = tc.get_user_by_username('codyw')
+print(json.dumps(data, indent=4))
+
+# data = tc.get_all_vcs_roots()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_vcs_root_by_vcs_root_id('CodeRepo')
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_build_types()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_changes_by_build_id(73450)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_build_statistics_by_build_id(73450)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_build_tags_by_build_id(73450)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_change_by_change_id(16884)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_changes()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_build_by_build_id(73450)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_projects()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_agents()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_agent_by_agent_id(41)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_build_type(build_type_id='Teamcity_TriggerFullBackup')
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_builds_by_build_type_id(build_type_id='Teamcity_TriggerFullBackup', start=0, count=3)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_builds(start=0, count=3)
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_plugins()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_server_info()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_all_projects()
+# print(json.dumps(data, indent=4))
+
+# data = tc.get_project_by_project_id('Usercontentsvc_ReleaseToMt5')
+# print(json.dumps(data, indent=4))
+
+# url = tc.get_project_by_project_id.get_url('Usercontentsvc_ReleaseToMt5')
+# print(url)
+
+# url = tc.get_project_by_project_id('Usercontentsvc_ReleaseToMt5', return_type='url')
+# print(url)
+
+# url = tc.get_project_by_project_id('Usercontentsvc_ReleaseToMt5', return_type='request')
+# print(url)
