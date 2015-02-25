a7fbf6aa60f2359009c4446c17030bd8a4043a97
Marc Abramowitz <marc@marc-abramowitz.com>
2015-02-25 11:14:32 -0800

    setup.py: Add more python version classifiers

--- setup.py
+++ setup.py
@@ -18,7 +18,12 @@ setup(
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Natural Language :: English',
         'Intended Audience :: Developers',
     ],
