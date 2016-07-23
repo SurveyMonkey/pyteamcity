86d7e0e2f13f090e47b3bf1bddb529a537b76c27
Oz Katz <ozkatz100@gmail.com>
2016-07-23 08:18:48 +0300

    Add HTTPS support (#24)
    * Add HTTPS support
    
    I've added a protocol parameter to support HTTPS enabled TeamCity servers.
    It could be configured by explicitly passing in `protocol` when initializing the class, or using the `TEAMCITY_PROTOCOL` environment variable.
    
    * added http as default protocol

--- pyteamcity.py
+++ pyteamcity.py
@@ -115,21 +115,23 @@ class TeamCity:
     error_handler = None
 
     def __init__(self, username=None, password=None, server=None, port=None,
-                 session=None):
+                 session=None, protocol=None):
         self.username = username or os.getenv('TEAMCITY_USER')
         self.password = password or os.getenv('TEAMCITY_PASSWORD')
         self.host = server or os.getenv('TEAMCITY_HOST')
         self.port = port or int(os.getenv('TEAMCITY_PORT', 0)) or 80
-        self.base_base_url = "http://%s:%d" % (self.host, self.port)
-        self.guest_auth_base_url = "http://%s:%d/guestAuth" % (
-            self.host, self.port)
+        self.protocol = protocol or os.getenv('TEAMCITY_PROTOCOL', 'http')
+        self.base_base_url = "%s://%s:%d" % (
+            self.protocol, self.host, self.port)
+        self.guest_auth_base_url = "%s://%s:%d/guestAuth" % (
+            self.protocol, self.host, self.port)
         if self.username and self.password:
-            self.base_url = "http://%s:%d/httpAuth/app/rest" % (
-                self.host, self.port)
+            self.base_url = "%s://%s:%d/httpAuth/app/rest" % (
+                self.protocol, self.host, self.port)
             self.auth = (self.username, self.password)
         else:
-            self.base_url = "http://%s:%d/guestAuth/app/rest" % (
-                self.host, self.port)
+            self.base_url = "%s://%s:%d/guestAuth/app/rest" % (
+                self.protocol, self.host, self.port)
             self.auth = None
         self.session = session or requests.Session()
         self._agent_cache = {}
