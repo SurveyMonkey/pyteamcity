9a6268923a583429552090fdeb9c868932d54bc1
Marc Abramowitz <marc@marc-abramowitz.com>
2016-08-10 07:54:36 -0700

    Add QueuedBuild.trigger_build

--- pyteamcity/future/queued_build.py
+++ pyteamcity/future/queued_build.py
@@ -1,3 +1,4 @@
+from . import exceptions
 from .core.parameter import Parameter
 from .core.queryset import QuerySet
 from .core.utils import parse_date_string
@@ -12,14 +13,17 @@ class QueuedBuild(WebBrowsable):
                  build_type_id,
                  queued_date_string,
                  branch_name, href, web_url,
-                 build_query_set, data_dict=None):
+                 build_query_set, teamcity, data_dict=None):
         self.id = id
         self.build_type_id = build_type_id
         self.queued_date_string = queued_date_string
         self.branch_name = branch_name
         self.href = href
         self.web_url = web_url
+        self.teamcity = teamcity
         self.build_query_set = build_query_set
+        if self.teamcity is None and self.build_query_set is not None:
+            self.teamcity = self.build_query_set.teamcity
         self._data_dict = data_dict
 
     @property
@@ -33,7 +37,7 @@ class QueuedBuild(WebBrowsable):
 
     @property
     def build_type(self):
-        teamcity = self.build_query_set.teamcity
+        teamcity = self.teamcity
         if 'buildType' in self._data_dict:
             build_type = BuildType.from_dict(self._data_dict.get('buildType'))
         elif 'buildTypeId' in self._data_dict:
@@ -50,7 +54,7 @@ class QueuedBuild(WebBrowsable):
             self.build_type_id)
 
     @classmethod
-    def from_dict(cls, d, build_query_set):
+    def from_dict(cls, d, build_query_set=None, teamcity=None):
         return cls(
             id=d.get('id'),
             build_type_id=d.get('buildTypeId'),
@@ -59,6 +63,7 @@ class QueuedBuild(WebBrowsable):
             href=d.get('href'),
             web_url=d.get('webUrl'),
             build_query_set=build_query_set,
+            teamcity=teamcity,
             data_dict=d)
 
     @property
@@ -106,3 +111,63 @@ class QueuedBuildQuerySet(QuerySet):
     def __iter__(self):
         return (self._entity_factory.from_dict(d, self)
                 for d in self._data().get('build', []))
+
+    def trigger_build(self,
+                      build_type_id, branch=None, comment=None,
+                      parameters=None, agent_id=None):
+        """
+        Trigger a new build
+        """
+        url = self.teamcity.base_base_url + self.uri
+        data = self._get_build_node(
+            build_type_id, branch,
+            comment, parameters, agent_id)
+
+        res = self.teamcity.session.post(
+            url,
+            headers={'Content-Type': 'application/xml'},
+            data=data)
+        if not res.ok:
+            raise exceptions.HTTPError(
+                status_code=res.status_code,
+                reason=res.reason,
+                text=res.text)
+
+        queued_build_data = res.json()
+        queued_build = QueuedBuild.from_dict(
+            queued_build_data,
+            teamcity=self.teamcity)
+        return queued_build
+
+    def _get_build_node(
+            self,
+            build_type_id, branch=None,
+            comment=None, parameters=None, agent_id=None):
+        build_attributes = ''
+
+        if branch:
+            build_attributes = 'branchName="%s"' % branch
+
+        if build_attributes:
+            data = '<build %s>\n' % build_attributes
+        else:
+            data = '<build>\n'
+
+        data += '    <buildType id="%s"/>\n' % build_type_id
+
+        if agent_id:
+            data += '    <agent id="%s"/>\n' % agent_id
+
+        if comment:
+            data += '    <comment><text>%s</text></comment>\n' % comment
+
+        if parameters:
+            data += '    <properties>\n'
+            data += ''.join([
+                '        <property name="%s" value="%s"/>\n' % (name, value)
+                for name, value in parameters.items()])
+            data += '    </properties>\n'
+
+        data += '</build>\n'
+
+        return data
