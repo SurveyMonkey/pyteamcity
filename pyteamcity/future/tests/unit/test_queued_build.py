2f7f4ca00bd4e459d691be7d25b3d779f57f6c52
Marc Abramowitz <marc@marc-abramowitz.com>
2016-08-12 10:43:31 -0700

    Add test_unit_queued_build_with_responses
    Increases test coverage to:
    
    ```
    Name                                                Stmts   Miss  Cover   Missing
    ---------------------------------------------------------------------------------
    pyteamcity/future/queued_build.py                     104     48    54%   31-32, 45-47, 73-83, 86-98, 120, 122, 124, 128, 141-160, 166-193
    ```

--- pyteamcity/future/tests/unit/test_queued_build.py
+++ pyteamcity/future/tests/unit/test_queued_build.py
@@ -1,6 +1,10 @@
+import datetime
+
+import responses
+
 from pyteamcity.future import TeamCity
 
-tc = TeamCity.from_environ()
+tc = TeamCity()
 
 
 def test_unit_all():
@@ -18,6 +22,94 @@ def test_unit_filter_by_project():
 
 def test_unit_filter_by_build_type():
     build_type = 'DevOps_Metacloud_DeleteOldVMs'
-    queued_builds = tc.queued_builds.all().filter(
-        build_type=build_type)
+    queued_builds = tc.queued_builds.all().filter(build_type=build_type)
     assert 'buildType:%s' % build_type in queued_builds._get_url()
+
+
+@responses.activate
+def test_unit_queued_build_with_responses():
+    response_json = {
+        "id": 1471658,
+        "buildTypeId": "Responseweb_2_Branches_Package",
+        "number": "2695",
+        "status": "UNKNOWN",
+        "state": "finished",
+        "branchName": "preview",
+        "href": "/httpAuth/app/rest/builds/id:1471658",
+        "webUrl": "https://tcserver/viewLog.html"
+                  "?buildId=1471658"
+                  "&buildTypeId=Responseweb_2_Branches_Package",
+        "statusText": "Canceled (Snapshot dependencies failed: 3 (new))",
+        "buildType": {
+            "id": "Responseweb_2_Branches_Package",
+            "name": "package",
+            "projectName": "responseweb :: branches",
+            "projectId": "Responseweb_2_Branches",
+            "href": "/httpAuth/app/rest/buildTypes"
+                    "/id:Responseweb_2_Branches_Package",
+            "webUrl": "https://tcserver/viewType.html"
+                      "?buildTypeId=Responseweb_2_Branches_Package",
+        },
+        "canceledInfo": {
+            "timestamp": "20160812T094513-0700",
+            "text": "Build was canceled"
+                    " because one of the builds it depends on failed",
+        },
+        "queuedDate": "20160812T094312-0700",
+        "startDate": "20160812T094513-0700",
+        "finishDate": "20160812T094513-0700",
+    }
+    response_list_json = {
+        "count": 2,
+        "href": "/httpAuth/app/rest/buildQueue/",
+        "build": [
+            {
+                "id": 1455869,
+                "buildTypeId": "Scansvc_PullRequests_Py27",
+                "state": "queued",
+                "branchName": "1/merge",
+                "href": "/httpAuth/app/rest/buildQueue/id:1455869",
+                "webUrl": "https://tcserver/viewQueued.html?itemId=1455869",
+            },
+            {
+                "id": 1471658,
+                "buildTypeId": "Responseweb_2_Branches_Package",
+                "state": "queued",
+                "branchName": "preview",
+                "href": "/httpAuth/app/rest/buildQueue/id:1471658",
+                "webUrl": "https://tcserver/viewQueued.html?itemId=1471658",
+            },
+        ],
+    }
+    responses.add(
+        responses.GET,
+        tc.relative_url('app/rest/buildQueue/'),
+        json=response_list_json, status=200,
+        content_type='application/json',
+    )
+    responses.add(
+        responses.GET,
+        tc.relative_url('app/rest/buildQueue/id:1471658'),
+        json=response_json, status=200,
+        content_type='application/json',
+    )
+
+    queued_builds = tc.queued_builds.all()
+    assert len(queued_builds) == 2
+    for queued_build in queued_builds:
+        assert hasattr(queued_build, 'build_type_id')
+        assert hasattr(queued_build, 'branch_name')
+        assert hasattr(queued_build, 'href')
+        assert hasattr(queued_build, 'web_url')
+    queued_build = tc.queued_builds.all().get(id=1471658)
+    assert 'id=1471658' in repr(queued_build)
+    assert 'Responseweb_2_Branches_Package' in repr(queued_build)
+    assert isinstance(queued_build.queued_date, datetime.datetime)
+    assert queued_build.queued_date.year == 2016
+    assert queued_build.queued_date.month == 8
+    assert queued_build.queued_date.day == 12
+    assert queued_build.queued_date.hour == 9
+    assert queued_build.queued_date.minute == 43
+    assert queued_build.queued_date.second == 12
+    assert queued_build.build_type.id == 'Responseweb_2_Branches_Package'
+    assert queued_build.build_type.name == 'package'
