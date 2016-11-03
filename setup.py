605c6e0e42a7d3f8aca20f4ce12127b27b5ba5d8
zacharya19 <zacharya19@gmail.com>
2016-11-03 10:58:07 +0200

    Fix setup package

--- setup.py
+++ setup.py
@@ -25,8 +25,7 @@ setup(
     maintainer_email='marc@marc-abramowitz.com',
     author='Yotam Oron',
     author_email='yotamoron@yahoo.com',
-    packages=['pyteamcity.legacy'],
-    py_modules=['pyteamcity'],
+    packages=['pyteamcity', 'pyteamcity.legacy'],
     zip_safe=False,
     install_requires=[
         'beautifulsoup4',
