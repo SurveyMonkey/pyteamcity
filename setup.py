4e2c479308918d1c7df40dc6063747205819bc9f
Bao Pham <gbaopham@gmail.com>
2017-02-12 22:00:55 +0800

    Add pyteamcity.future.core to packages

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     maintainer_email='marc@marc-abramowitz.com',
     author='Yotam Oron',
     author_email='yotamoron@yahoo.com',
-    packages=['pyteamcity', 'pyteamcity.legacy', 'pyteamcity.future'],
+    packages=['pyteamcity', 'pyteamcity.legacy', 'pyteamcity.future', 'pyteamcity.future.core'],
     zip_safe=False,
     install_requires=[
         'beautifulsoup4',
